a424496e88fc3b3586a6b21f799b0ae6badefc94
gregoryreis <greis003@fiu.edu>
2026-01-14 16:11:26 -0500

    Adding a new project on the YoungFolks store

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+from pprint import pprint
+
+products = requests.get("https://fakestoreapi.com/products").json()
+
+print(f"There are {len(products)} products in the 'YoungFolks' store.")
+
+for p in products:
+    print("Title:", p["title"])
+    print("Category:", p["category"])
+    print()
+
+#TODO: include the rate (not the count) of the product
+#TODO: include the price
+#TODO: include the picture of the product
